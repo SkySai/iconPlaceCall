7a084c550c2fc4078577c4361042f481d4907187
Vincent He <vincent.dexter.he@gmail.com>
2023-03-01 10:26:36 -0600

    Add files via upload

---
+++ placeCall.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+auth="admin:admin"
+PROGNAME=$(basename "$0")
+if [ "$#" -eq 2 ]; then
+    ipFile=$1
+	extension=$2
+	echo "using default password"
+elif [ "$#" -eq 3 ]; then
+    auth="admin:${1}"
+    ipFile=$2
+	extension=$3
+else
+    echo "USAGE: placeCall [auth] ipFile extension"
+    echo "auth: specify username and password when non default (admin:admin) include the colon to separate the username from the password"
+    echo "ipFile: specify filename with list of IPs"
+    exit 1
+fi
+sed -e :a -e '/^\n*$/{$d;N;};/\n$/ba' $ipFile > temp
+while IFS="" read -r line || [ -n "$line" ]
+do
+ ip=$line
+ echo "AUTH is ${auth}"
+ b64auth=$(echo -n ${auth} | base64)
+ echo "IP is ${ip}"
+
+ session=$( curl -s -H "Authorization: LSBasic ${b64auth}" -H "Content-Type: application/json" http://$ip/rest/new | awk -F\" '/session/ { print $4 }' 2> /dev/null )
+# echo "SESSION is ${session}"
+
+ curl -H "Authorization: LSBasic ${b64auth}" -H "Content-Type: application/json" --data "{\"params\":{\"number\":${extension}},\"call\":\"Conf_dialNumber\"}" http://${ip}/rest/request/${session} 
+
+done <"temp"
+rm temp
